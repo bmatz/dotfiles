68ebb2035803c52a570aecb9e1ac641a95781477
Bernhard Matz <bernhard@trigo.at>
2016-09-25 04:28:08 +0200

    autoupdate repo

--- setup-docker.sh
+++ setup-docker.sh
@@ -5,7 +5,7 @@ sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 rm /etc/apt/sources.list.d/docker.list
-cp docker.list /etc/apt/sources.list.d/docker.list
+sudo cp docker.list /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
 
